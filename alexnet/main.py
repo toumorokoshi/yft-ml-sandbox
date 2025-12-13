70d507acbba0f7079bb2148bbbebe39fc85f3f35
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2025-12-12 22:40:50 -0800

    adding compositions

--- alexnet/main.py
+++ alexnet/main.py
@@ -3,39 +3,89 @@ import torch
 import torch.nn as nn
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from torchvision.transforms import v2
 from torch.utils.data import DataLoader
 
 BATCH_SIZE = 64
+# Imagenette has varying images sizes by default,
+# so we need to choose a resized one for consistency.
+SIZE = "320px"
 
 def main(argv: list[str]) -> None:
     print("This is the main function of AlexNet.")
 
     # Download training data from open datasets.
-    #training_data = datasets.ImageNet(
-    #    root="data",
-    #    train=True,
-    #    download=True,
-    #    transform=ToTensor(),
-    #)
-#
+    training_data = datasets.Imagenette(
+        root="data",
+        split="train",
+        size=SIZE,
+        download=True,
+        transform=ToTensor(),
+    )
+
     ## Download test data from open datasets.
-    #test_data = datasets.ImagetNet(
-    #    root="data",
-    #    train=False,
-    #    download=True,
-    #    transform=ToTensor(),
-    #)
-#
-    #train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE)
-    #test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
+    test_data = datasets.Imagenette(
+        root="data",
+        split="val",
+        size=SIZE,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE)
+    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
     device = (
         torch.accelerator.current_accelerator().type
         if torch.accelerator.is_available() else "cpu"
     )
+    loss_fn = nn.CrossEntropyLoss()
     print(f"Using device: {device}")
     model = NeuralNetwork().to(device)
     print(f"Model structure: {model}")
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
 
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 # Define model
 class NeuralNetwork(nn.Module):
@@ -54,6 +104,8 @@ class NeuralNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+
+
 
 if __name__ == "__main__":
     main(sys.argv)
