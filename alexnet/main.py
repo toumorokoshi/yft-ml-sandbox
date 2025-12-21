4d853d3e28aa0a7f25afd0cfeee8b32dfe4c8cf6
Yusuke Tsutsumi <yusuke.tsutsumi@getcruise.com>
2025-12-20 21:48:13 -0800

    feat(alexnet): building full alexnet

--- alexnet/main.py
+++ alexnet/main.py
@@ -1,6 +1,7 @@
 import sys
 import torch
 import torch.nn as nn
+import asciichartpy
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torchvision.transforms import v2
@@ -64,13 +65,19 @@ def main(argv: list[str]) -> None:
     model = NeuralNetwork().to(device)
     print(f"Model structure: {model}")
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
-    epochs = 20
+    # TODO: add weight decay of 0.0005
+    epochs = 60
     model.train()
+    test_accuracies = []
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer, device)
+        test_accuracy = test(test_dataloader, model, loss_fn, device)
+        test_accuracies.append(test_accuracy)
+    print(f"Test accuracies: {test_accuracies}")
+    ascii_chart = asciichartpy.plot(test_accuracies, {"height": 20})
+    print(ascii_chart)
     model.eval()
-    test(test_dataloader, model, loss_fn, device)
     print("Done!")
 
 
@@ -93,7 +100,7 @@ def train(dataloader, model, loss_fn, optimizer, device):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def test(dataloader, model, loss_fn, device):
+def test(dataloader, model, loss_fn, device) -> float:
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -108,33 +115,69 @@ def test(dataloader, model, loss_fn, device):
             # therefore handles the final transform to a single value per batch.
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
-    correct /= size
+    accuracy = 100 * (correct / size)
     print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+        f"Test Error: \n Accuracy: {(accuracy):>0.1f}%, Avg loss: {test_loss:>8f} \n"
     )
+    return accuracy
 
 
 # Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        middle_layer_size = 28 * 28 * 28 // 32  # results in ~ 2gb
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(IMAGE_HEIGHT * IMAGE_WIDTH * 3, middle_layer_size),
+        # middle_layer_size = 28 * 28 * 28 // 32  # 686, results in ~ 2gb
+        # conv_output_height = ((IMAGE_HEIGHT - 11) // 4 + 1) - 4
+        # conv_output_width = ((IMAGE_WIDTH - 11) // 4 + 1) - 4
+        conv_features = [96, 256, 384, 384, 256]
+        # conv_output_size = 52224 # with maxpool
+        conv_output_size = 38400 # with maxpool * 2
+        # conv_output_size = conv_features[-1] * conv_output_height * conv_output_width
+        #print(f"Conv output size: {conv_output_size}")
+        # TO TRY:
+        # - maxpooling between conv layers
+        self.conv_stack = nn.Sequential(
+            # the output of a conv layer is a 3D tensor, where:
+            # - the input tensor is shrunk by the kernel size minus 1 (since the convolution
+            #   has to act on a fill matrix).
+            # - the input tensor is divided by the stride, since you will get that many
+            #   fewer points.
+            # - the output is the number of channels desired.
+            nn.Conv2d(3, conv_features[0], kernel_size=11, stride=4, padding=0),
             nn.ReLU(),
-            nn.Linear(middle_layer_size, 512),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),  # section 3.3
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(conv_features[0], conv_features[1], kernel_size=5, stride=1, padding=0),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),  # section 3.3
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0),
+            nn.Conv2d(conv_features[1], conv_features[2], kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(conv_features[2], conv_features[3], kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(conv_features[3], conv_features[4], kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+        )
+
+        self.linear_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(0.5),
+            nn.Linear(conv_output_size, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
             nn.ReLU(),
             # The last layer generally should be a linear layer, not an
             # activation function like a ReLU, which will bound the output
             # unnescessarily and impact the fidelity into a function like
             # softmax, which can consume between negative and positive infinity.
-            nn.Linear(512, NUM_CLASSIFICATIONS),
+            nn.Linear(4096, NUM_CLASSIFICATIONS),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        conv_output = self.conv_stack(x)
+        # print(f"Conv output shape: {conv_output.shape}")
+        logits = self.linear_stack(conv_output)
         return logits
 
 
