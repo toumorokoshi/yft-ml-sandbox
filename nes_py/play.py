0d412135fbe6465203563c1b3b9cce51abadf13a
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2026-07-19 22:07:26 -0700

    fix: address stages=None TypeError in play.py environment creation
    Reimplement the gym-super-mario-bros CLI launcher in play.py to avoid passing stages=None to gym.make. Modern gym (0.26+) passes all extra parameters directly to the environment constructor, causing a TypeError when stages is provided to non-RandomStages environments.
    
    Modified areas:
    - nes_py/play.py: Implement the custom parser and initialization logic, omitting stages from gym.make unless it is explicitly specified.

--- nes_py/play.py
+++ nes_py/play.py
@@ -16,31 +16,80 @@ if "RUNFILES_DIR" not in os.environ:
         sys.path.remove("")
 
 try:
-    from gym_super_mario_bros._app.cli import main as gym_mario_main
+    import gym
+    import gym_super_mario_bros
+    from nes_py.wrappers import JoypadSpace
+    from nes_py.app.play_human import play_human
+    from nes_py.app.play_random import play_random
+    from gym_super_mario_bros.actions import RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 except ImportError as e:
-    print("Error: Could not import gym_super_mario_bros. Ensure the package is installed.", file=sys.stderr)
+    print("Error: Could not import dependencies. Ensure packages are installed.", file=sys.stderr)
     sys.exit(1)
-
-
 finally:
     # Restore original path
     sys.path = sys_path_backup
 
+# Key mapping of action spaces
+_ACTION_SPACES = {
+    'right': RIGHT_ONLY,
+    'simple': SIMPLE_MOVEMENT,
+    'complex': COMPLEX_MOVEMENT,
+}
+
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Wrapper to run gym_super_mario_bros CLI")
-    parser.add_argument("--env", "-e", default="SuperMarioBros-v0", help="The environment ID")
-    parser.add_argument("--mode", "-m", default="human", choices=["human", "random"], help="The control mode")
+    parser = argparse.ArgumentParser(description="Play Super Mario Bros using gym-super-mario-bros")
+    parser.add_argument('--env', '-e',
+        type=str,
+        default='SuperMarioBros-v3',
+        help='The name of the environment to play'
+    )
+    parser.add_argument('--mode', '-m',
+        type=str,
+        default='human',
+        choices=['human', 'random'],
+        help='The execution mode for the emulation'
+    )
+    parser.add_argument('--actionspace', '-a',
+        type=str,
+        default='nes',
+        choices=['nes', 'right', 'simple', 'complex'],
+        help='the action space wrapper to use'
+    )
+    parser.add_argument('--steps', '-s',
+        type=int,
+        default=500,
+        help='The number of random steps to take.',
+    )
+    parser.add_argument('--stages', '-S',
+        type=str,
+        nargs='+',
+        help='The random stages to sample from for a random stage env'
+    )
 
-    # Parse args to intercept them and translate if necessary
-    args, unknown = parser.parse_known_args()
+    args = parser.parse_args()
 
-    # Reconstruct sys.argv to call gym_super_mario_bros CLI main
-    # gym_super_mario_bros expects: -e <env> -m <mode>
-    sys.argv = [sys.argv[0], "-e", args.env, "-m", args.mode] + unknown
+    if args.stages is not None and 'RandomStages' not in args.env:
+        print('--stages,-S should only be specified for RandomStages environments', file=sys.stderr)
+        sys.exit(1)
 
-    # Delegate execution to gym_super_mario_bros CLI main
-    gym_mario_main()
+    # Replicate gym.make call but FIX the stages parameter issue!
+    # If stages is None, do NOT pass it to gym.make!
+    if args.stages is not None:
+        env = gym.make(args.env, stages=args.stages)
+    else:
+        env = gym.make(args.env)
+
+    # Wrap the environment with action space if specified
+    if args.actionspace != 'nes':
+        actions = _ACTION_SPACES[args.actionspace]
+        env = JoypadSpace(env, actions)
+
+    # Play the environment
+    if args.mode == 'human':
+        play_human(env)
+    else:
+        play_random(env, args.steps)
 
 
 if __name__ == "__main__":
